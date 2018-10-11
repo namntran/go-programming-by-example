304ec3e89d5bdd9b0d8d93e616aecf11f8dfcecd
namntran <namtran78@gmail.com>
2018-10-12 08:11:08 +1000

    add example lesson printing verbs

---
+++ ch2/ex2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	concepts()
+}
+func concepts() {
+
+	// fmt.Println("JohnJack")
+	// fmt.Println("John\tJack")
+	i := 10
+	fmt.Printf("%d %x %T", i, i, i)
+
+}
